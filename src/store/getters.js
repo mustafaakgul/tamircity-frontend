ca560ec24b463032221c86f81d2d707c07040c52
sonerardansisman <ardansisman@atlasyazilim.com.tr>
2022-07-20 17:16:36 +0300

    Add tab disable in vuex

---
+++ src/store/getters.js
@@ -0,0 +1,7 @@
+export const getBrandDisabled = (state) => {
+    return state.tabDisabled.brand;
+}
+
+export const getModelDisabled = (state) => {
+    return state.tabDisabled.model;
+}
--- src/store/modules/Brand.js
+++ src/store/modules/Brand.js
@@ -1,30 +1,49 @@
-const state ={
-    brands:[]
+const state = {
+  brands: [],
+
 }
 
+const getters = {
+  getStateBrands(state) {
+    return state.brands;
+  },
+};
+
 const mutations = {
-    getBrand(state, payload) {
-      state.brands = payload;
-    },
-   
-  };
-  
-  const actions = {
-    getBrands({ commit }, payload) {
-        axios.get("http://localhost:8080/api/v1/brand?device_type_id="+ payload.device_type_id)
-        .then(response => {
-            console.log("brand response : " + response.data);
-            commit("getBrand", response.data);
-        })
-        .catch(e => console.log(e));  
-      
-    },
-   
-  };
-  
-  export default {
-    state,
-    getters,
-    mutations,
-    actions,
-  };
+  getBrand(state, payload) {
+    console.log("getBrand mutations worked.")
+    console.log(payload)
+    payload.forEach((item, index) => {
+      //console.log(item + "-" + index)
+       let test = { id : index,text:item.Text}
+      state.brands.push(test)
+    })
+    //state.brands = payload;
+
+  },
+
+};
+
+const actions = {
+  getBrands({ commit, rootState }, payload) {
+    console.log("getBrands action worked " + payload.id + " " + payload.text)
+    rootState.tabDisabled.brand = false;
+    let brandList = [{ Id: 1, Text: "Apple" }, { Id: 2, Text: "Samsung" }]
+    commit("getBrand", brandList);
+    // axios.get("http://localhost:8080/api/v1/brand?device_type_id="+ payload.device_type_id)
+    // .then(response => {
+    //     console.log("brand response : " + response.data);
+    //     commit("getBrand", response.data);
+    // })
+    // .catch(e => console.log(e));  
+
+  },
+
+};
+
+export default {
+  getters,
+  state,
+  mutations,
+  actions,
+};
