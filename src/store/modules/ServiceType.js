39eac377f29b8d209d19f395cab3243c4c2e1063
hatice <hatice@hotmail.com>
2022-09-18 22:53:05 +0300

    completed servistype api integration

--- src/store/modules/ServiceType.js
+++ src/store/modules/ServiceType.js
@@ -14,7 +14,7 @@ const mutations = {
     getServiceType(state, payload) {
     state.serviceTypes = [];
     payload.forEach((item, index) => {
-      let test = { id: index, text: item.Text }
+      let test = { id: index, text: item.name }
       state.serviceTypes.push(test)
     })     
     NextStep("fixType", "serviceType");
@@ -28,15 +28,12 @@ const actions = {
     console.log("getServiceType action worked " + payload.id + " " + payload.text)
     console.log("Rootstate SelectedItem serviceType : " + rootState.selectedItems.serviceType)
     rootState.tabDisabled.serviceType = false;
-    let serviceTypeList = [{ Id: 1, Text: "Yerinde Tamir" }, { Id: 2, Text: "Merkezde Tamir" }, { Id: 3, Text: "Kargo" }, { Id: 4, Text: "Kurye" }]
-    commit("getServiceType", serviceTypeList);
-
-    // axios.get("http://localhost:8080/api/v1/brand?device_type_id="+ payload.device_type_id)
-    // .then(response => {
-    //     console.log("fixTypes response : " + response.data);
-    //     commit("getFixType", response.data);
-    // })
-    // .catch(e => console.log(e));  
+     this.axios.get("http://167.172.105.3:8000/api/v1/service-types")
+     .then(response => {
+        console.log("getServiceType response : " + response.data.data);
+         commit("getServiceType", response.data.data);
+     })
+    .catch(e => console.log(e));
 
   },
 
