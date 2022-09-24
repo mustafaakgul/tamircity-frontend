72652fc35840fcbe9ac6ae2bcc485e1164720740
hatice <hatice@hotmail.com>
2022-09-25 02:16:50 +0300

    List Page

--- src/views/pages/pages/services.vue
+++ src/views/pages/pages/services.vue
@@ -4,98 +4,87 @@
   <div v-for="service in getStateTechServices" :key="service" class="row">
     <div class="col-md-12">
       <div class="tab-content" id="myTabContent">
-        <div
-          class="tab-pane fade show active"
-          id="new_bookings"
-          role="tabpanel"
-          aria-labelledby="new-bookings"
-        >
+        <div class="tab-pane fade show active" id="new_bookings" role="tabpanel" aria-labelledby="new-bookings">
           <div class="card guide-booking-table mb-0">
             <div class="card-body p-0">
               <div class="table-responsive">
                 <table class="table table-center mb-0">
                   <tbody>
-                    <tr>
-                      <td>
-                        <div class="table-booking d-flex align-items-center">
-                          <div class="table-booking-info">
-                            <router-link to="">
-                              {{ service.text }}
-                            </router-link>
-                            <p>
-                              <i class="fas fa-clock me-1"></i> 14 Nov 2022,
-                              10.00 AM
-                            </p>
+                  <tr>
+                    <td>
+                      <div class="table-booking d-flex align-items-center">
+                        <div class="table-booking-img">
+                          <router-link to="/guide-settings">
+                            <img class="avatar me-2 avatar-img" src="../../../assets/img/profiles/avatar-04.jpg" alt="User Image">
+                          </router-link>
+                        </div>
+                        <div class="table-booking-info">
+                          <router-link to="">
+                            {{ service.text }}
+                          </router-link>
+                          <p>14 Nov 2022, 10.00 AM</p>
+                          <div class="comment-date">
+                            <p class="text-primary mb-0"><i class="fas fa-clock me-1"></i> August 13, 2022 13:03 pm</p>
+                            <div class="review-count rating">
+                              <i class="fas fa-star filled"></i>
+                              <i class="fas fa-star filled"></i>
+                              <i class="fas fa-star filled"></i>
+                              <i class="fas fa-star filled"></i>
+                              <i class="fas fa-star"></i>
+                              <span class="d-inline-block average-rating">(04)</span>
+                            </div>
                           </div>
                         </div>
-                      </td>
-                      <td>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="table-booking-address d-flex align-items-center">
+                        Newyork, United States
                         <div
-                          class="
+                            class="
                             table-booking-address
                             d-flex
                             align-items-center
                           "
                         >
-                          <span><i class="feather-map-pin"></i></span>
                           {{ service.address }}
                         </div>
-                      </td>
-                      <td>
-                        <div
-                          class="
-                            table-booking-address
-                            d-flex
-                            align-items-center
-                          "
-                        >
-                          <span><i class="feather-mail"></i></span>
-                          georgeanderson@example.com
-                        </div>
-                      </td>
-                      <td>
-                       <div class="table-booking-btn">
-                        <a
-                            href="javascript:void(0);"
-                            class="btn booking-btn-workTime"
-                            data-bs-toggle="modal" data-bs-target="#work-time"
-                          >
-                            <i class="far fa-circle-check me-1"></i> Saat Seçiniz
-                          </a>
-                         
-                       </div>           
-                          <!-- <work-time-modal></work-time-modal> -->
-                        
-                      </td>
-                      <td>
-                        <div class="table-booking-payment">
-                          <span>$ 120/210</span>
-                          <p>Partially Paid</p>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="table-booking-btn">
-                          <a
-                            href="javascript:void(0);"
-                            class="btn booking-btn-accept"
-                          >
-                            <i class="far fa-circle-check me-1"></i> Accept
-                          </a>
-                          <a
-                            href="javascript:void(0);"
-                            class="btn booking-btn-cancel mb-0"
-                          >
-                            <i class="far fa-circle-xmark me-1"></i> Cancel
-                          </a>
-                        </div>
-                      </td>
-                    </tr>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="table-booking-address d-flex align-items-center">
+                        georgeanderson@example.com
+                      </div>
+                    </td>
+                    <td>
+                      <div class="table-booking-address d-flex align-items-center">
+                        +1 923 782 4575
+                      </div>
+                    </td>
+                    <td>
+                      <div class="table-booking-payment">
+                      <span>$ 120/210</span>
+                        <p>Partially Paid</p>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="table-booking-btn">
+                        <a href="javascript:void(0);" class="btn booking-btn-accept">
+                         Accept
+                        </a>
+                        <a href="javascript:void(0);" class="btn booking-btn-cancel mb-0">
+                           Cancel
+                        </a>
+                      </div>
+                    </td>
+                  </tr>
                   </tbody>
                 </table>
               </div>
             </div>
           </div>
         </div>
+
       </div>
     </div>
   </div>
