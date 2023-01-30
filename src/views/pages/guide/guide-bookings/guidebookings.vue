5eb0204a74ee53bda9a6aeb8ff6bc66ce66a0a93
mustafakocatepe <msaidkocatepe@gmail.com>
2023-01-30 21:23:20 +0300

    add button

--- src/views/pages/guide/guide-bookings/guidebookings.vue
+++ src/views/pages/guide/guide-bookings/guidebookings.vue
@@ -129,10 +129,10 @@
                           </div>
                         </td>
                         <td>
-                          <div class="table-booking-completed">
-                            <span>
-                                <i class="far fa-circle-check me-1"></i> Completed
-                            </span>
+                         <div class="table-booking-btn">
+                            <a href="javascript:void(0);" class="btn booking-btn-accept">
+                              <i class="far fa-circle-info me-1"></i> Forma Git
+                            </a>
                           </div>
                         </td>
                       </tr>
@@ -286,26 +286,26 @@ export default {
     },
     methods: {
         getBookingApproved() {
-          axios.get('http://localhost:8888/api/v1/reservations/approved?expertise_service_id=1')
+          axios.get('http://157.230.124.187:8888/api/v1/reservations/approved?expertise_service_id=1')
               .then(response => {
                 this.approvedBookings = response.data.data
               })
         },
         getBookingCancelled() {
-          axios.get('http://localhost:8888/api/v1/reservations/cancelled?expertise_service_id=1')
+          axios.get('http://157.230.124.187:8888/api/v1/reservations/cancelled?expertise_service_id=1')
               .then(response => {
                 this.cancelledBookings = response.data.data
               })
         },
         getBookingPending() {
-          axios.get('http://localhost:8888/api/v1/reservations/pending?expertise_service_id=1')
+          axios.get('http://157.230.124.187:8888/api/v1/reservations/pending?expertise_service_id=1')
               .then(response => {
                 console.log(response.data.data)
                 this.pendingBookings = response.data.data
               })
         },
         getBookingCompleted() {
-          axios.get('http://localhost:8888/api/v1/reservations/completed?expertise_service_id=1')
+          axios.get('http://157.230.124.187:8888/api/v1/reservations/completed?expertise_service_id=1')
               .then(response => {
                 console.log(response.data.data)
                 this.completedBookings = response.data.data
