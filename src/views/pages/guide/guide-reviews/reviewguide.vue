0584cc2bb48f02ce02708a73e3600dcfaa5dd6cf
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-03-10 12:04:31 +0300

    add vuex store

--- src/views/pages/guide/guide-reviews/reviewguide.vue
+++ src/views/pages/guide/guide-reviews/reviewguide.vue
@@ -4,7 +4,8 @@
     <div class="col-md-12">
         <div class="guide-review review-listing">
             <ul class="comments-list">
-                <li>
+
+                <li v-for="item in comments" :key="item.id">
                     <div class="comment">
                         <div class="review-profile">
                             <router-link to="/guide-details">
@@ -32,93 +33,30 @@
                         </div>
                     </div>
                 </li>
-                <li>
-                    <div class="comment">
-                        <div class="review-profile">
-                            <router-link to="/guide-details">
-                                <img src="../../../../assets/img/provider/provider-04.jpg" class="avatar" alt="Provider Image">
-                            </router-link>
-                        </div>
-                        <div class="comment-body">
-                            <div class="meta-data">
-                                <router-link to="/guide-details">Bernadine</router-link>
-                            </div>
-                            <p class="comment-content">
-                                Et Harum Quidem Rerum Facilis Est Et Expedita Distinctio. Nam Libero Tempore, Cum Soluta Nobis Est Eligendi Optio Cumque Nihil Impedit Quo Minus Id Quod Maxime Placeat Facere Possimus
-                            </p>
-                            <div class="comment-date">
-                                <p class="text-primary mb-0"><i class="fas fa-clock me-1"></i> September 25, 2022 24:10 pm</p>
-                                <div class="review-count rating">
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <span class="d-inline-block average-rating">(03)</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="comment">
-                        <div class="review-profile">
-                            <router-link to="/guide-details">
-                                <img src="../../../../assets/img/provider/provider-06.jpg" class="avatar" alt="Provider Image">
-                            </router-link>
-                        </div>
-                        <div class="comment-body">
-                            <div class="meta-data">
-                                <router-link to="/guide-details">Kau Teren</router-link>
-                            </div>
-                            <p class="comment-content">
-                                Et Harum Quidem Rerum Facilis Est Et Expedita Distinctio. Nam Libero Tempore, Cum Soluta Nobis Est Eligendi Optio Cumque Nihil Impedit Quo Minus Id Quod Maxime Placeat Facere Possimus
-                            </p>
-                            <div class="comment-date">
-                                <p class="text-primary mb-0"><i class="fas fa-clock me-1"></i> October 08, 2022 14:05 am</p>
-                                <div class="review-count rating">
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star"></i>
-                                    <span class="d-inline-block average-rating">(04)</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="comment">
-                        <div class="review-profile">
-                            <router-link to="/guide-details">
-                                <img src="../../../../assets/img/provider/provider-07.jpg" class="avatar" alt="Provider Image">
-                            </router-link>
-                        </div>
-                        <div class="comment-body">
-                            <div class="meta-data">
-                                <router-link to="/guide-details">James Harrem</router-link>
-                            </div>
-                            <p class="comment-content">
-                                Et Harum Quidem Rerum Facilis Est Et Expedita Distinctio. Nam Libero Tempore, Cum Soluta Nobis Est Eligendi Optio Cumque Nihil Impedit Quo Minus Id Quod Maxime Placeat Facere Possimus
-                            </p>
-                            <div class="comment-date">
-                                <p class="text-primary mb-0"><i class="fas fa-clock me-1"></i> December 08, 2022 14:05 am</p>
-                                <div class="review-count rating">
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star filled"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <span class="d-inline-block average-rating">(03)</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+
             </ul>
         </div>
     </div>
 </div>
 <!-- /Guide Reviews -->
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "ReviewGuide",
+    data() {
+        return {
+            comments: [],
+        };
+    },
+    mounted() {
+        axios
+            .get("https://jsonplaceholder.typicode.com/comments")
+            .then((response) => {
+                this.comments = response.data;
+            });
+    },
+};
+</script>
